810d45aff21895e32d693eadfdd3a57eed5889cd
ArenDeveloper05 <antisystem005@mail.ru>
2024-06-13 19:17:33 +0400

    .

--- src/components/home/home-services/HomeServices.jsx
+++ src/components/home/home-services/HomeServices.jsx
@@ -1,4 +1,8 @@
+import { Link } from "react-router-dom";
 import { homeServicesConfig } from "../../../config";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { ROUTER } from "../../../router/router";
+
 import HomeServicesItem from "./home-services-item/HomeServicesItem";
 
 import "./HomeServices.scss";
@@ -6,7 +10,12 @@ import "./HomeServices.scss";
 const HomeServices = () => {
   return (
     <section className="home-services">
-      <h1 className="section-title">Услуги</h1>
+      <h1 className="section-title">
+        <Link to={ROUTER.SERVICES_PAGE_ROUTE}>
+          Услуги
+          <MdKeyboardArrowRight />
+        </Link>
+      </h1>
       <div className="home-services-list">
         {homeServicesConfig.map(({ id, title, link, description }) => {
           return (
